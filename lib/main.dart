2b1f984035b66f3e4811259ed77ddc6e87eb3ca5
shubhankar pandey <shubhankarpandey961@gmail.com>
2021-07-17 12:16:37 +0530

    Bluetooth Chat App Initital

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:bluetooth_app/bluetooth.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: Bluetooth(),
+  ));
+}
+
+
